57dabcef829dddcb11b8a0705aa6c0eb7df0f3e5
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-05-01 16:52:17 +1000

    added sequencing to genning Qs (add team details not working)

--- index.js
+++ index.js
@@ -17,46 +17,78 @@ const askManagerQuestions = () => {
 
 const askEngineerQuestions = () => {
     questions = q.engineerQuestions();
-    return inquirer.prompt(questions);
+    return inquirer.prompt(questions)
+        // .then(answers => {
+        //     console.log("engineer");
+        //     console.log(answers);
+
+        //     addTeamMember();
+        // })
 }
 
-// const addTeamMemberQuestions = () => {
-//     return [
-//         {
-//             type: "list",
-//             name: "teamMember",
-//             message: "Add a team member?",
-//             choices: ["Engineer", "Intern", "I do not want to add a team member."]
-//         }
-//     ];
-// }
+const askInternQuestions = () => {
+    questions = q.internQuestions();
+    return inquirer
+        .prompt(questions)
+        // .then(answers => {
+        //     console.log(answers);
+
+        //     addTeamMember();
+        // });
+}
 
 const addTeamMember = () => {
-    questions = [ 
-        {
-            type: "list",
-            name: "teamMember",
-            message: "Add a team member?",
-            choices: ["Engineer", "Intern", "I do not want to add a team member."]
-        }
-    ]
-    return inquirer.prompt(questions);
+    questions = {
+        type: "list",
+        name: "role",
+        message: "Add a team member?",
+        choices: ["Engineer", "Intern", "I do not want to add a team member."]
+    }
+
+    return inquirer.prompt(questions)
+        .then(answers => {
+            // console.log("add team member answer:");
+            console.log(answers)
+            
+            teamMemberDetails(answers.role);
+        })
+}
+
+const teamMemberDetails = (role) => {
+    switch (role) {
+        case "Engineer":
+            askEngineerQuestions();
+            break;
+        case "Intern":
+            askInternQuestions();
+            break;
+        default:
+            exitAndGeneratePage();
+    }
+}
+
+const exitAndGeneratePage = () => {
+    console.log("exit");
 }
 
 const init = () => {
     askManagerQuestions()
         .then(answers => {
-            console.log(answers);
+            // console.log("the answers object:");
+            // console.log(answers);
 
             const manager = new Manager(
                 `${answers.manager.name}`,
                 `${answers.manager.id}`,
                 `${answers.manager.email}`,
                 `${answers.manager.officeNumber}`)
-                
-            team.push(manager);
 
-            console.log(team);
+            team.push(manager);
+            
+            // console.log("the team array:");
+            // console.log(team);
+
+            addTeamMember();
         })
 }
 
